abe5f70eea60233085c44488a238a59aa55bcfc6
thisisrandom069069 <thisisrandom069069@gmail.com>
2023-04-24 18:38:03 +0000

    test1

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
-
+const currenturl = document.URL
 form.addEventListener('submit', async event => {
     event.preventDefault();
     window.navigator.serviceWorker.register('./sw.js', {
@@ -9,7 +9,7 @@ form.addEventListener('submit', async event => {
         let url = input.value.trim();
         if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-        var iurl = document.URL + __uv$config.prefix + __uv$config.encodeUrl(url);
+        var iurl = currenturl + __uv$config.prefix + __uv$config.encodeUrl(url);
         var win = window.open()
         var iframe = win.document.createElement('iframe')
         iframe.style.width = "100%";
