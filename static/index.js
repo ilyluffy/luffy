c44bfeb39052698aa331fe28fcf2f136f6e9d14b
thisisrandom069069 <thisisrandom069069@gmail.com>
2023-04-24 18:36:35 +0000

    test

--- static/index.js
+++ static/index.js
@@ -9,8 +9,14 @@ form.addEventListener('submit', async event => {
         let url = input.value.trim();
         if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-
-
+        var iurl = document.URL + __uv$config.prefix + __uv$config.encodeUrl(url);
+        var win = window.open()
+        var iframe = win.document.createElement('iframe')
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.border = "none";
+        iframe.src = iurl
+        win.document.body.appendChild(iframe)
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
     });
 });
